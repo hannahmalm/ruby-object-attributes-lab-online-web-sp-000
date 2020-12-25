3653535ff299b0b34138c8973b7e7b3cbdaaf9f1
Hannah Malm <hannahemalm616@gmail.com>
2020-12-25 20:40:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,16 @@
-	Def initialize(first_name, last_name)
-	@first_name = first_name
-	@last_name = last_name
-	End 
-	 
-	Def name=(full_name)
-	Fist_name, last_name = full_name.split	@first_name = first_name
-	@last_name = last_name
-	End 
-	 
-	Def name 
-	“#{@first_name} #{@last_name}”.strip
-	End  
-	End 
+class Dog
+	def name 
+	  
+	end 
+	
+	def name= 
+	  
+	end  
+	
+	def breed 
+	  
+	end 
+	
+	def breed= 
+	  
+	end   
